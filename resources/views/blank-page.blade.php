3b32e50cd30768303ed5ccaba6783c9e50120013
bahri <saiful.bahri.tl@gmail.com>
2023-08-10 21:20:55 +0700

    add cwb

--- resources/views/blank-page.blade.php
+++ resources/views/blank-page.blade.php
@@ -12,7 +12,7 @@
     <div class="main-content">
         <section class="section">
             <div class="section-header">
-                <h1>Dashboard FIC 7 - Biznet GioCloud</h1>
+                <h1>Dashboard FIC 7 - Biznet GioCloud x Code with Bahri</h1>
             </div>
             <div class="row">
                 <div class="col-lg-3 col-md-6 col-sm-6 col-12">
